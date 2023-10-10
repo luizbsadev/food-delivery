a9883b898d1114d5e6650c7e13be17c94567776b
luizbsadev <luiz.bsa.olv@gmail.com>
2023-10-10 08:59:59 -0300

    Spring initializr

---
+++ fooddelivery/src/main/java/com/bsa/fooddelivery/FooddeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.bsa.fooddelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooddeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooddeliveryApplication.class, args);
+	}
+
+}
